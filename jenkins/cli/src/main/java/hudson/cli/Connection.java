68df9c50cc2621eb4e3874032f9e5cd0a86c51e9
Craig Scarboro <57534046+cbsc228@users.noreply.github.com>
2020-10-30 17:33:23 -0400

    q5

--- jenkins/cli/src/main/java/hudson/cli/Connection.java
+++ jenkins/cli/src/main/java/hudson/cli/Connection.java
@@ -145,28 +145,42 @@ public class Connection {
     public KeyAgreement diffieHellman(boolean side) throws IOException, GeneralSecurityException {
         return diffieHellman(side,512);
     }
+	public KeyPair generateKeyPairWithSpec(DHParameterSpec paramSpec, KeyPair keyPair){
+		try{
+			KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
+			dh.initialize(paramSpec);
+			keyPair = dh.generateKeyPair();
+			return keyPair;
+		}catch(Exception e){
+			return null;
+		}
+		
+	}
+	
     public KeyAgreement diffieHellman(boolean side, int keySize) throws IOException, GeneralSecurityException {
-        KeyPair keyPair;
+        KeyPair keyPair = null;
         PublicKey otherHalf;
 
         if (side) {
             AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
             paramGen.init(keySize);
-
+/*
             KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
             dh.initialize(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
             keyPair = dh.generateKeyPair();
-
+*/
+			keyPair = generateKeyPairWithSpec(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class), keyPair);
             // send a half and get a half
             writeKey(keyPair.getPublic());
             otherHalf = KeyFactory.getInstance("DH").generatePublic(readKey());
         } else {
             otherHalf = KeyFactory.getInstance("DH").generatePublic(readKey());
-
+/*
             KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DH");
             keyPairGen.initialize(((DHPublicKey) otherHalf).getParams());
             keyPair = keyPairGen.generateKeyPair();
-
+*/
+			keyPair = generateKeyPairWithSpec(((DHPublicKey) otherHalf).getParams(), keyPair);
             // send a half and get a half
             writeKey(keyPair.getPublic());
         }
